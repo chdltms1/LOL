0776edbbe1f913c05d7a78ee845b705e28c56ba7
chdltms1 <61855480+chdltms1@users.noreply.github.com>
2020-03-10 14:21:36 +0900

    챔프 정보
    정보를 표시

---
+++ src/code/champion.java
@@ -0,0 +1,23 @@
+package code;
+
+public class champion {
+	
+	
+	String name;
+	String position;
+	String secondPosition;
+	int price;
+	
+	void championInfomation() {
+		
+		System.out.println("이름 : " +name);
+		System.out.println("포지션 : " +position);
+		System.out.println("부 포지션 : " +secondPosition);
+		System.out.println("이름 : " +price);
+		
+		
+		
+	}
+	
+
+}
